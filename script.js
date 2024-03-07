690a1aaff5d17dc63e33a073e7019dec04112b1a
viano <162569579+vianoforguson@users.noreply.github.com>
2024-03-07 18:03:40 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-const navBtn = document.querySelector("#navbar-toggler");
-const navDiv = document.querySelector(".navbar-collapse");
+ let menu = document.querySelector('#menu-btn');
+ let navbar = document.querySelector('.header .navbar')
+menu.onclick = () => {
+    menu.classList.toggle('ri-xrp-line');
+    navbar.classList.toggle('active');
+}
 
-navBtn.addEventListener("click", () => {
-  navDiv.classList.toggle("showNav");
+menu.onscroll = () =>{
+    menu.classList.remove('ri-xrp-line')
+    navbar.classList.remove('active')
+}
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Menghubungi melalui WhatsApp
+//     document.getElementById("whatsappbtn").addEventListener("click", function () {
+//       window.location.href =
+//         "https://api.whatsapp.com/send?phone=6282131323598&text=Halo%2C%20saya%20tertarik%20dengan%20leptop%20jenis%20asus%20Anda";
+//     });
+//   });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Menghubungi melalui WhatsApp
+  document.body.addEventListener("click", function (event) {
+      if (event.target.classList.contains("whatsappbtn")) {
+          var phone = event.target.getAttribute("data-phone");
+          var message = encodeURIComponent(event.target.getAttribute("data-message"));
+          window.location.href = "https://api.whatsapp.com/send?phone=" + phone + "&text=" + message;
+      }
+  });
 });
-
-// stopping animation and transition during window resizing
-let resizeTimer;
-window.addEventListener("resize", () => {
-  document.body.classList.add("resize-animation-stopper");
-  clearTimeout(resizeTimer);
-  resizeTimer = setTimeout(() => {
-    document.body.classList.remove("resize-animation-stopper");
-  }, 400);
-});
-
-const sr = ScrollReveal({
-  origin:'top',
-  distance:'85px',
-  duration:'2500',
-  reset:true
-});
-
-sr.reveal ('.home-text',{delay:300});
-sr.reveal ('.row-left',{delay:400});
-sr.reveal ('.container',{delay:400});
-
-sr.reveal ('.abaout-img',{});
-sr.reveal ('.abaout-text',{delay:300});
-
-sr.reveal ('.middle-text',{});
-sr.reveal ('.row-btn,.shop-content',{delay:300});
-
-sr.reveal ('.review-content,.content',{delay:300});
